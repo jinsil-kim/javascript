dc0134ddadb8e36a1e93ad1cb2f0dc9e47f95e6f
jinsil-kim <wlstlf9315@gmail.com>
2024-07-19 14:54:45 +0900

    Add files via upload

---
+++ Day01/hello.js
@@ -0,0 +1,6 @@
+// window.alert("오늘은 자바스크립트 배우는날");
+
+//window.alert("오늘 점심은 샤브샤브먹고싶음");
+window.console.log("주말에는 집에만 있을거임");
+window.console.log("오늘 저녁 집밥 ");
+window.console.log("타자를 다 쳐야되는게 굉장히 귀찮음");
---
+++ Day01/memory/index.js
@@ -0,0 +1,20 @@
+// const name = "jinsil";
+// const coffee = "latte";
+
+// window.console.log(name);
+// window.console.log(coffee);
+// window.console.log("내 이름은 name 좋아하는 커피는 coffee입니다.");
+// window.console.log(`내 이름은 ${name} 좋아하는 커피는 ${coffee}입니다.`);
+
+// const mbti = "infp";
+// const weekend = "집에서 뒹굴뒹굴 할거에요";
+// const youtube = "숏츠 하루종일 볼겁니다";
+
+// window.console.log(
+//   `제 mbti는 ${mbti}이고, 주말에는 ${weekend}, 유튜브는 ${youtube}`
+// );
+
+const phone = window.prompt("내 폰은?");
+const pc = window.prompt("내 컴은?");
+
+window.console.log(`내가 쓰는 핸드폰은 ${phone}이고 내 pc는 ${pc}입니다`);
